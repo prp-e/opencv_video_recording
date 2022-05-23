b85d4c8317cedd55da7f30a7d180590a6adbe142
Muhammadreza Haghiri <haghiri75@gmail.com>
2022-05-23 17:24:43 +0430

    Successfully wrote video on an avi file

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import cv2
 from cv2 import VideoWriter, VideoWriter_fourcc
 
 camera = cv2.VideoCapture(1)
+video = VideoWriter('output.avi', VideoWriter_fourcc(*'MJPG'), 10, (1280,720))
 
 while camera.isOpened():
     _, frame = camera.read()
-
+    video.write(frame)
+    
     cv2.imshow('Footage', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
